97add133f9b1cb8de7c97e3205b8c3b0ebaee12d
Valery <kharvd@gmail.com>
2011-02-19 09:41:53 +0300

    Bumped version number to 0.9.3

--- src/exampleswidget.h
+++ src/exampleswidget.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-** WordTrain 0.9.2 -- Foreign words trainer
+** WordTrain 0.9.3 -- Foreign words trainer
 ** Copyright (C) 2010  Valery Kharitonov <kharvd@gmail.com>
 **
 ** This file is part of WordTrain.
